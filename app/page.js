b10259ab78c4d1ef67bd973e56f51b2728e3bf43
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2024-10-27 18:18:47 -0300

    tentando2

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import "./globals.css"
 export default function Home() {
   return (
     <div className="center">
-
+      <div className="popup-inicial">
+        <h2>Antes de começar a jogatina.</h2>
+        <p>Nosso simulador de Caixar se passa em um mundo imaginário e fictício. Se quiser pode se contextualizar com ele vendo o video de introdução ou pular.</p>
+                        <iframe src="https://www.youtube.com/embed/zmu51Xlh3tc" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
+      </div>
     </div>
   );
 }
